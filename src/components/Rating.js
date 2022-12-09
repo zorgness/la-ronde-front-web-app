3c3a226cb32bd19f04cbd7dcef29a4897c6b4d8c
Thomas Fouquert <tfouquert@gmail.com>
2022-12-09 11:32:39 +0100

    add rating on istruments

---
+++ src/components/Rating.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Rating = ({rating}) => {
+  console.log(rating)
+  const score = (rating / 5) * 100
+
+  return (
+    <span className="star-wrapper">
+      <span className="stars" style={{ width: score + "%" }}></span>
+    </span>
+  )
+}
+
+export default Rating
--- src/components/userComponents/Instrument.js
+++ src/components/userComponents/Instrument.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { importAll } from '../../utils/importAll';
+import Rating from '../Rating';
 
 const Instrument = ({instruments}) => {
 
    const images = importAll(require.context('../../images/instru_images', false, /\.(png|jpe?g|svg)$/));
+
+   function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
 
   return (
 
@@ -16,6 +23,7 @@ const Instrument = ({instruments}) => {
                 <li key={id} className="list-group-item text-start " style={{border: "none"}}>
                   <img src={images[`${name}.png`]} alt={name} className='avatar-square'/>
                   <span className='mx-5'>{name}</span>
+                  <Rating rating={getRandomIntInclusive(1, 5)} />
                 </li>
              )
           })
