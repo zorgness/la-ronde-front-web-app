37a33fa5421d6f78c09019eb2adc0902c04caa4f
Thomas Fouquert <tfouquert@gmail.com>
2022-12-10 10:21:21 +0100

    update

--- src/components/userComponents/Profile.js
+++ src/components/userComponents/Profile.js
@@ -6,6 +6,7 @@ import ModalDescription from "./ModalDescription";
 import MaterialsContainer from "../userComponents/MaterialsContainer";
 import { userMaterials } from "../Data/userMaterials";
 import { Link } from "react-router-dom";
+import arrow from "../../images/arrow.png";
 
 const Profile = ({ userData, userInstrus }) => {
   const [show, setShow] = useState(false);
@@ -17,12 +18,15 @@ const Profile = ({ userData, userInstrus }) => {
     <div id="profile">
       <p className="text-end mx-3">{userData.city}</p>
 
-      <Link
-        to={`/musician-profile/${userData.id}`}
-        className="btn btn-outline-dark"
-      >
-        See my public profile
-      </Link>
+      <div className="text-start m-3">
+        <Link
+          to={`/musician-profile/${userData.id}`}
+          className="btn btn-outline-primary"
+        >
+          <img src={arrow} alt="" className="avatar-small mx-2" />
+          public profile
+        </Link>
+      </div>
       <h3 className="text-start mx-5">Mes Instruments</h3>
 
       <div>
